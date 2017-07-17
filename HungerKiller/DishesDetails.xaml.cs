97dd82c368e672088dcc55430862933778405ddd
SakuraSong001 <syy_0601@126.com>
2017-07-17 09:13:04 +0800

    菜品详细

--- HungerKiller/DishesDetails.xaml.cs
+++ HungerKiller/DishesDetails.xaml.cs
@@ -16,7 +16,7 @@ using Windows.UI.Xaml.Navigation;
 // https://go.microsoft.com/fwlink/?LinkId=234238 上介绍了“空白页”项模板
 
 namespace HungerKiller
-{
+{    
     /// <summary>
     /// 可用于自身或导航至 Frame 内部的空白页。
     /// </summary>
@@ -25,6 +25,31 @@ namespace HungerKiller
         public DishesDetails()
         {
             this.InitializeComponent();
+        }
+
+        private void Wanting_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Ate_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SpitSlotButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void HottestComment_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void NewestComment_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
