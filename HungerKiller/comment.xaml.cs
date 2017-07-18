ab3ca6ae6ae44742bf66ed3b6aa4f91186f27a50
WRLYHGF <729750541@qq.com>
2017-07-18 19:28:43 +0800

    改掉一个错误

--- HungerKiller/comment.xaml.cs
+++ HungerKiller/comment.xaml.cs
@@ -20,9 +20,9 @@ namespace HungerKiller
     /// <summary>
     /// 可用于自身或导航至 Frame 内部的空白页。
     /// </summary>
-    public sealed partial class Commment : Page
+    public sealed partial class comment : Page
     {
-        public Commment()
+        public comment()
         {
             this.InitializeComponent();
         }
