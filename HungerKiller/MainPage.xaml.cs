b0739cd45198851cc209779b56b4e1be498a4c3b
SakuraSong001 <syy_0601@126.com>
2017-07-20 09:49:23 +0800

    一点小问题

--- HungerKiller/MainPage.xaml.cs
+++ HungerKiller/MainPage.xaml.cs
@@ -44,93 +44,136 @@ namespace HungerKiller
                 Setting_1.Visibility = Visibility.Visible;
                 SettingListBox_2.Visibility = Visibility.Collapsed;
             }
+            if(Setting_1.Visibility==Visibility.Visible)
+            {                
+                if(UserName.Width<=67)
+                {
+                    IfUserNameSoLong.Visibility = Visibility.Collapsed;
+                }
+                else
+                {
+                    IfUserNameSoLong.Visibility = Visibility.Visible;
+                }
+            }
         }
      
 
         private void BaseListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
+        {            
             if (HamburgerButton.IsSelected)
             {
+                ManegerListBox.SelectedIndex = SettingListBox_2.SelectedIndex = -1;
                 MySplitView.IsPaneOpen = !MySplitView.IsPaneOpen;
                 IfSplitViewOpen();
             }
             else if (Homepage.IsSelected)
             {
+                ManegerListBox.SelectedIndex = SettingListBox_2.SelectedIndex = -1;
                 MyFrame.Navigate(typeof(homepage));
                 IfSplitViewOpen();
             }
             else if (Deal.IsSelected)
             {
+                ManegerListBox.SelectedIndex = SettingListBox_2.SelectedIndex = -1;
                 MyFrame.Navigate(typeof(deal));
                 IfSplitViewOpen();
             }
         }
 
         private void ManegerListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
+        {           
             if (Maneger.IsSelected)
             {
+                BaseListBox.SelectedIndex = SettingListBox_2.SelectedIndex = -1;
                 MyFrame.Navigate(typeof(Maneger));
                 IfSplitViewOpen();
             }
-        }
-
-       
-       
-        /*  SplitView 打开时点击用户头像，这是个button_click事件*/
-        /* 横版的用户登录入口*/
-        private void MyUserHeaderImageButton_1_Click(object sender, RoutedEventArgs e)
-        {
-            Sign_In jump = new Sign_In();
-            jump.ShowAsync();
-            
-        }
-
-        /// <summary>
-        /// 点击未登录提示符，可以跳转至登录或注册页面
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void IfNotSignIn_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        /// <summary>
-        /// 点击用户名，可以打开个人信息页面
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void UserName_Click(object sender, RoutedEventArgs e)
-        {
-
         }
 
 
         /*  SplitView 关闭时点击用户头像、或者设置按钮，这是个ListBoxSelectionChanged事件*/
         /*竖版的用户登录入口及设置按钮*/
         private void SettingListBox_2_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
+        {            
             if(MyUserHeaderImage_2.IsSelected)
             {
+                ManegerListBox.SelectedIndex = BaseListBox.SelectedIndex = -1;
+                UserName_Click(sender,e);
                 //判断已经登录？ 否，则打开登录页面
                 /*MyFrame.Navigate(typeof(SignIn));//SignIn为登录界面*/
                 //已登录则打开个人资料侧滑框
             }
+            else if(MySettingButton_2.IsSelected)
+            {
+                ManegerListBox.SelectedIndex = BaseListBox.SelectedIndex = -1;
+                MySettingSplitView.IsPaneOpen = !MySettingSplitView.IsPaneOpen;
+                SelfInfoSplitView.IsPaneOpen = false;
+            }
         }
-
+        /// <summary>
+        /// 设置按钮
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void MySettingButton_Click(object sender, RoutedEventArgs e)
         {
-            // MyFrame.Navigate(typeof(Setting));
+            MySettingSplitView.IsPaneOpen = !MySettingSplitView.IsPaneOpen;
+            SelfInfoSplitView.IsPaneOpen = false;
         }
 
-        /*
-private void LoggedButton_Click(object sender, RoutedEventArgs e)
-{
+        /// <summary>
+        /// 点击用户名或头像位置，若已登录可以打开个人信息页面，否则打开登录页面
+        /// SplitView 打开时点击用户头像，这是个button_click事件
+        /// 横版的用户登录入口
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void UserName_Click(object sender, RoutedEventArgs e)
+        {
+            ManegerListBox.SelectedIndex = BaseListBox.SelectedIndex = -1;
+            //if(未登录)
+            Sign_In jump = new Sign_In();
+            jump.ShowAsync();
+            /*
+             else
+             {
+                
+                MySettingSplitView.IsPaneOpen = false;
+                SelfInfoSplitView.IsPaneOpen = !SelfInfoSplitView.IsPaneOpen;
+            }
+             */
+        }
 
-MyFrame.Navigate(typeof(LoginPage));
-TitleTextBlock.Text = "登录";
-}
-*/
+
+
+        //以下是右侧侧滑框内函数
+
+        /// <summary>
+        /// 账号、设备安全
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void AccountSecuritySetting_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        /// <summary>
+        /// 消息与隐私设置
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MessagePrivacySetting_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        /// <summary>
+        /// 清除缓存
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ClearCacheSetting_Click(object sender, RoutedEventArgs e)
+        {
+           
+        }
     }
 }
